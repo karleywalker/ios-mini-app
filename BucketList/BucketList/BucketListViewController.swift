a2e86c57b56ca1d19fa68ce7221e8ce1e826cb6d
hcd2ha <hcd2ha@virinia.edu>
2017-02-12 23:36:03 -0500

    segue
    add button goes to add page

---
+++ BucketList/BucketList/BucketListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BucketListViewController.swift
+//  BucketList
+//
+//  Created by Hanna Dickerson on 2/12/17.
+//  Copyright © 2017 Hanna Dickerson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class BucketListViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
