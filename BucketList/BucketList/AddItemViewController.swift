c53672a0a80e0100ecbfe63737f1227c82c5b4e8
hcd2ha <hcd2ha@virinia.edu>
2017-02-19 18:05:11 -0500

    BucketListTableViewController

--- BucketList/BucketList/AddItemViewController.swift
+++ BucketList/BucketList/AddItemViewController.swift
@@ -2,22 +2,34 @@
 //  AddItemViewController.swift
 //  BucketList
 //
-//  Created by Hanna Dickerson on 2/12/17.
+//  Created by Hanna Dickerson on 2/19/17.
 //  Copyright © 2017 Hanna Dickerson. All rights reserved.
 //
 
-import Foundation
 import UIKit
-class AddItemViewController:UIViewController {
-    
+
+class AddItemViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
