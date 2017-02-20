ff042db7227a08b86e1c1b30d061f2d0bb522399
hcd2ha <hcd2ha@virinia.edu>
2017-02-19 21:35:42 -0500

    Issues

--- BucketList/BucketList/BucketListTableViewCell.swift
+++ BucketList/BucketList/BucketListTableViewCell.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Hanna Dickerson. All rights reserved.
 //
 
-import Foundation
+
 
 import UIKit
 
--- BucketList/BucketList/BucketListTableViewController.swift
+++ BucketList/BucketList/BucketListTableViewController.swift
@@ -23,6 +23,16 @@ class BucketListTableViewController: UITableViewController {
         bucketItems += [item4]
     }
         
+    //MARK: Button Actions
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Code for you to write!", message: "You can add code here to open a new ViewController to add a new note!", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -49,26 +59,70 @@ class BucketListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        //PROBLEM PROBLEM PROBLEM!!!!!!
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "BucketListTableViewCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BucketListTableViewCell
+        
+        // Fetches the appropriate note for the data source layout.
+        let item = bucketItems[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        let convertedDate = dateFormatter.string(from: item.date)
+        cell.dateLabel.text = convertedDate
+        
         return cell
     }
-    */
+    
+    // Override to support tapping on an element in the table view.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath[1]
+        let currentItem = bucketItems[index]
+        let alertController = UIAlertController(title: currentItem.name, message: currentItem.desc + "\nsaved at: " + currentItem.date.description, preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+ 
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
+    
+    // Lets you add various buttons when you swipe
+    override func tableView(_ tableView: UITableView, editActionsForRowAt: IndexPath) -> [UITableViewRowAction]? {
+        let done = UITableViewRowAction(style: .normal, title: "Done") { action, index in
+            
+            
+        }
+        done.backgroundColor = .green
+        
+        let edit = UITableViewRowAction(style: .normal, title: "Edit") { action, index in
+            
+            
+        }
+        edit.backgroundColor = .orange
+        
+        
+        return [done, edit]
+    }
 
     /*
     // Override to support editing the table view.
