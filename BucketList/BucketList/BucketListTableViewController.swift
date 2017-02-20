8e7d3dd9972c4ca7c1dd3e1786e0f17f0a3a4c13
hcd2ha <hcd2ha@virinia.edu>
2017-02-20 13:22:33 -0500

    Initially sorted
    The initial list is sorted by date

--- BucketList/BucketList/BucketListTableViewController.swift
+++ BucketList/BucketList/BucketListTableViewController.swift
@@ -21,14 +21,26 @@ class BucketListTableViewController: UITableViewController {
     }
     
     func loadSampleItems() {
-        let item1 = BucketItem(name: "Hike Humpback at sunrise", desc: "Take a panoramic picture of all those trees.", latitude:1.1, longitude: 1.1, date: Date())
+        
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        
+        let date1: Date = dateFormatterGet.date(from: "2018-03-29 12:24:26")!
+        let date2: Date = dateFormatterGet.date(from: "2018-02-28 12:24:26")!
+        let date3: Date = dateFormatterGet.date(from: "2018-05-29 12:24:26")!
+        let date4: Date = dateFormatterGet.date(from: "2018-01-29 12:24:26")!
+        
+        let item1 = BucketItem(name: "Hike Humpback at sunrise", desc: "Take a panoramic picture of all those trees.", latitude:1.1, longitude: 1.1, date: date1 as Date)
         bucketItems += [item1]
-        let item2 = BucketItem(name: "Make a mobile app", desc: "It's gotta be a bucket list.", latitude:2.2, longitude: 2.2, date: Date())
+        let item2 = BucketItem(name: "Make a mobile app", desc: "It's gotta be a bucket list.", latitude:2.2, longitude: 2.2, date: date2 as Date)
         bucketItems += [item2]
-        let item3 = BucketItem(name: "Register to graduate", desc: "Make sure you get a diploma", latitude:3.3, longitude: 3.3, date: Date())
+        let item3 = BucketItem(name: "Register to graduate", desc: "Make sure you get a diploma", latitude:3.3, longitude: 3.3, date: date3 as Date)
         bucketItems += [item3]
-        let item4 = BucketItem(name: "Get a bagel in Rice", desc: "They're delicious", latitude:4.4, longitude: 4.4, date: Date())
+        let item4 = BucketItem(name: "Get a bagel in Rice", desc: "They're delicious", latitude:4.4, longitude: 4.4, date: date4 as Date)
         bucketItems += [item4]
+        
+        //sort by date
+        bucketItems.sort(by: { $0.date < $1.date })
     }
     
     //MARK: Button Actions
